64d2cb4c667e080435839659ac201856e2397ce6
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-23 21:28:12 +0530

    add counter

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const prevBtn = document.querySelector(".prevBtn");
 slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
+
+let counter = 0;
+nextBtn.addEventListener("click", function () {
+  counter++;
+});
+prevBtn.addEventListener("click", function () {
+  counter--;
+});
