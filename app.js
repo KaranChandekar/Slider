7a8956f29c4325c8f9a63ab83f03f06ba9e63d1d
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-23 21:36:56 +0530

    working with slides

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ prevBtn.addEventListener("click", function () {
 });
 
 function carousel() {
+  // working with slides
+  if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slid.left - 1;
+  }
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
